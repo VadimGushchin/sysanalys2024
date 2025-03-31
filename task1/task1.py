1e79b7734ae369475bf21cc5fc0cc4f06541ee9f
semiotia <gushchvadim@yandex.ru>
2025-03-31 15:59:09 +0300

    task1

---
+++ task1/task1.py
@@ -0,0 +1,12 @@
+##Run python3 task1.py ../example.csv 'row' 'column' ##
+
+import csv
+import sys
+
+data = sys.argv[1]
+row = int(sys.argv[2])
+column = int(sys.argv[3])
+
+with open(data, newline='') as csvfile:
+    csv_table = list(csv.reader(csvfile, delimiter=','))
+    print(csv_table[row - 1][column - 1])
